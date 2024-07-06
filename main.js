dfc2ebaf7cbd9d103235bc1c562123efdf648439
Your Name <you@example.com>
2024-07-06 18:56:58 -0300

    basico

--- main.js
+++ main.js
@@ -2,20 +2,17 @@ function tocaSom(idElementoAudio){
     document.querySelector (idElementoAudio).play()
 }
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador = 0;
-
-//while = enquanto  length = comprimento
-while (contador < listaDeTeclas.length){
+//while = enquanto  length = comprimento   contador++ = a mesma coisa que contador + 1.
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     //template string
     const idAudio = `#som_${instrumento}`;
-    console.log(idAudio)
-
+    //console.log(idAudio)
     tecla.onclick = function (){ 
         tocaSom(idAudio);
     }
-    contador = contador + 1; //contador é igual contador + 1 pois se não adicionar o +1 o variavel contador ira começar em 0 assim ocasionando um erro.
-   // console.log(contador) show the process on console browser
+    //contador = contador + 1; //contador é igual contador + 1 pois se não adicionar o +1 o variavel contador ira começar em 0 assim ocasionando um erro.
+   //console.log(contador) //mostra o processo no console do navegador.
 }
